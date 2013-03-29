2db8528d689584b9c384d593baece6f9dd6645d4
Yulia <yulia@DenverVB.(none)>
2013-03-29 12:51:45 -0400

    Weekly Tunes controller and view

--- app/controllers/weekly_tunes_controller.rb
+++ app/controllers/weekly_tunes_controller.rb
@@ -1,2 +1,81 @@
+
 class WeeklyTunesController < ApplicationController
+  # GET /weekly_tunes
+  # GET /weekly_tunes.json
+  def index
+    @weekly_tunes = WeeklyTune.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @weekly_tunes }
+    end
+  end
+
+  def show
+    @weekly_tune = WeeklyTune.find(params[:id])
+    @uri = URI(@weekly_tune.description)
+    @u = "http://www.youtube.com/embed" + @uri.path.to_s
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @weekly_tune }
+    end
+  end
+
+  def new
+    @weekly_tune = WeeklyTune.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @weekly_tune }
+    end
+  end
+
+  # GET /weekly_tunes/1/edit
+  def edit
+    @weekly_tune = WeeklyTune.find(params[:id])
+  end
+
+  # POST /weekly_tunes
+  # POST /weekly_tunes.json
+  def create
+    @weekly_tune = WeeklyTune.new(params[:weekly_tune])
+
+    respond_to do |format|
+      if @weekly_tune.save
+        format.html { redirect_to @weekly_tune, notice: 'Weekly tune was successfully created.' }
+        format.json { render json: @weekly_tune, status: :created, location: @weekly_tune }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @weekly_tune.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /weekly_tunes/1
+  # PUT /weekly_tunes/1.json
+  def update
+    @weekly_tune = WeeklyTune.find(params[:id])
+
+    respond_to do |format|
+      if @weekly_tune.update_attributes(params[:weekly_tune])
+        format.html { redirect_to @weekly_tune, notice: 'Weekly tune was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @weekly_tune.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /weekly_tunes/1
+  # DELETE /weekly_tunes/1.json
+  def destroy
+    @weekly_tune = WeeklyTune.find(params[:id])
+    @weekly_tune.destroy
+
+    respond_to do |format|
+      format.html { redirect_to weekly_tunes_url }
+      format.json { head :no_content }
+    end
+  end
 end
--- app/models/weekly_tune.rb
+++ app/models/weekly_tune.rb
@@ -1,6 +1,3 @@
 class WeeklyTune < ActiveRecord::Base
   attr_accessible :artist, :date_from, :date_to, :description, :genre, :title
-
-  belongs_to :user
-  has_many :comments
 end
