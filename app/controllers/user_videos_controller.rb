feca869d6ff892437f81a68fec12b75301b4359c
Christine Lytwynec <chris.lytwynec@gmail.com>
2013-03-29 11:32:13 -0400

    user model updates and css/bootstrap

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -1,2 +1,82 @@
 class UserVideosController < ApplicationController
+
+   def index
+    @videos = current_user.user_videos.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @videos }
+    end
+  end
+
+  # GET /videos/1
+  # GET /videos/1.json
+  def show
+    @video = current_user.user_videos.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @video }
+    end
+  end
+
+  # GET /videos/new
+  # GET /videos/new.json
+  def new
+    @video = current_user.user_videos.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @video }
+    end
+  end
+
+  # GET /videos/1/edit
+  def edit
+    @video = current_user.user_videos.find(params[:id])
+  end
+
+  # POST /videos
+  # POST /videos.json
+  def create
+    @video = current_user.user_videos.new(params[:video])
+
+    respond_to do |format|
+      if @video.save
+        format.html { redirect_to @video, notice: 'Task was successfully created.' }
+        format.json { render json: @video, status: :created, location: @video }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /videos/1
+  # PUT /videos/1.json
+  def update
+    @video = current_user.user_videos.find(params[:id])
+
+    respond_to do |format|
+      if @video.update_attributes(params[:video])
+        format.html { redirect_to @video, notice: 'Task was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /videos/1
+  # DELETE /videos/1.json
+  def destroy
+    @video = current_user.user_videos.find(params[:id])
+    @video.destroy
+
+    respond_to do |format|
+      format.html { redirect_to videos_url }
+      format.json { head :no_content }
+    end
+  end
 end
--- app/models/user_video.rb
+++ app/models/user_video.rb
@@ -1,6 +1,6 @@
 class UserVideo < ActiveRecord::Base
-  attr_accessible :description, :title, :video_embed
+  attr_accessible :description, :title, :video_embed, :user_id
 
-  belongs_to :users
+  belongs_to :user
   has_many :comments
 end
