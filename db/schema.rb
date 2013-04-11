3e4af271a9af5157c96e02b1fa85b97c5d68965c
Yulia <liatetskaya@gmail.com>
2013-04-10 22:43:43 -0400

    Comments for User_Video

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130325190917) do
+ActiveRecord::Schema.define(:version => 20130411014037) do
 
   create_table "comments", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.string   "name"
+    t.integer  "user_video_id"
   end
 
   create_table "instruments", :force => true do |t|
